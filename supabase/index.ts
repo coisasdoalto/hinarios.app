49bb22240517464773cc764e36a4d261d514169c
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2024-12-14 11:55:38 -0300

    chore: add script to generate supabase types

--- supabase/index.ts
+++ supabase/index.ts
@@ -2,7 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 
 import { Database } from './database.types';
 
-const supabaseUrl = 'https://tldfezkopcqqowoucbdo.supabase.co';
+export const SUPABASE_PROJECT_ID = 'tldfezkopcqqowoucbdo';
+
+const supabaseUrl = `https://${SUPABASE_PROJECT_ID}.supabase.co`;
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsZGZlemtvcGNxcW93b3VjYmRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NDY3ODcsImV4cCI6MjAzNTAyMjc4N30.p4PtTNvzZNdpDrqeRzuYX9ye8YS3_Xks1b4nRDg054c';
 
