4d894f72136fff7f454d7e6536f855a0fa464e5b
PabloDinella <pablo.dinella@gmail.com>
2024-12-31 15:48:59 -0300

    Add feature flag to nearby songs

--- components/NearbySongs/index.tsx
+++ components/NearbySongs/index.tsx
@@ -1,14 +1,20 @@
 import { Affix, ActionIcon, Drawer, NavLink } from '@mantine/core';
-import { IconRadar2 } from '@tabler/icons';
+import { IconGlobeFilled } from '@tabler/icons-react';
 import { useProximityHymns } from 'hooks/useProximityHymns';
 import Link from 'next/link';
+import { useFeatureFlagEnabled } from 'posthog-js/react';
 import { useState } from 'react';
-import LazyLoad from 'react-lazyload';
 
 export function NearbySongs() {
   const { data: proximityHymns } = useProximityHymns();
 
   const [opened, setOpened] = useState(false);
+
+  const isNearbySongsEnabled = useFeatureFlagEnabled('nearby-songs');
+
+  if (!isNearbySongsEnabled) {
+    return null;
+  }
 
   return (
     <>
@@ -20,7 +26,7 @@ export function NearbySongs() {
         zIndex={2}
       >
         <ActionIcon onClick={() => setOpened(true)} radius="xl" variant="filled" size={60}>
-          <IconRadar2 />
+          <IconGlobeFilled />
         </ActionIcon>
       </Affix>
 
@@ -35,19 +41,6 @@ export function NearbySongs() {
           <NavLink
             key={item.hymn_slug}
             label={`${item.hymn_number} - ${item.hymn_title}`}
-            //   description={item.subtitle}
-            // rightSection={
-            //   <Group>
-            //     <IconStar size={16} stroke={1.5} />
-            //     <IconHeadphones size={16} stroke={1.5} onClick={() => console.log('play')} />
-            //   </Group>
-            // }
-            //   icon={
-            //     <Text sx={{ minWidth: 29, textAlign: 'right' }} size="sm">
-            //       {item.number}
-            //     </Text>
-            //   }
-            //   onClick={() => setActive(index)}
             component={Link}
             href={`/${item.hymn_book_slug}/${item.hymn_slug}`}
             onClick={() => setOpened(false)}
