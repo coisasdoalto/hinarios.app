bb105d046d49ec5dcf87085ebb3df8444da027b3
PabloDinella <pablo.dinella@gmail.com>
2025-03-31 23:02:58 -0300

    Remove search news

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,25 @@
-import { Alert, Card, Collapse, Container, Group, Text } from '@mantine/core';
+import { Card, Group, Text } from '@mantine/core';
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
 import { useHymnBooksSave } from '../context/HymnBooks';
 import getHymnBooks from '../data/getHymnBooks';
 import { HymnBook } from '../schemas/hymnBook';
-import { IconAlertCircle } from '@tabler/icons';
-import { useLocalStorage } from '@mantine/hooks';
-import { usePostHog } from 'posthog-js/react';
 
 type PageProps = { hymnBooks: HymnBook[] };
 
 export default function Home({ hymnBooks }: PageProps) {
   useHymnBooksSave(hymnBooks);
 
-  const posthog = usePostHog();
-
-  const [dismissed, setDismissed] = useLocalStorage({
-    key: 'UpdateNewSearchDimissed',
-    deserialize: (value) => Boolean(value),
-    defaultValue: false,
-  });
-
   return (
-    <>
-      <Collapse in={!dismissed}>
-        <Container size="xs" mb={35}>
-          <Alert
-            icon={<IconAlertCircle size={16} />}
-            title="Nova busca!"
-            color="indigo"
-            withCloseButton
-            onClose={() => {
-              setDismissed(true);
-              posthog.capture('dismiss_new_search_click');
-            }}
-          >
-            Implementamos um <strong>novo sistema de busca</strong>, mais rápido e mais preciso!
-            Caso encontre algum problema deixe um <strong>feedback abaixo</strong>.
-          </Alert>
-        </Container>
-      </Collapse>
-
-      <Group position="center">
-        {hymnBooks.map((hymnBook) => (
-          <Card key={hymnBook.slug} shadow="sm" p="xl" component={Link} href={`/${hymnBook.slug}`}>
-            <Text weight={500} size="lg" m={0}>
-              {hymnBook.name}
-            </Text>
-          </Card>
-        ))}
-      </Group>
-    </>
+    <Group position="center">
+      {hymnBooks.map((hymnBook) => (
+        <Card key={hymnBook.slug} shadow="sm" p="xl" component={Link} href={`/${hymnBook.slug}`}>
+          <Text weight={500} size="lg" m={0}>
+            {hymnBook.name}
+          </Text>
+        </Card>
+      ))}
+    </Group>
   );
 }
 
