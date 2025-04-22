c6d9b908bf58bc54b916399eb37d8f6a2805fb23
PabloDinella <pablo.dinella@gmail.com>
2025-04-22 17:43:16 -0300

    Search by number

--- data/getHymnBooks.ts
+++ data/getHymnBooks.ts
@@ -1,6 +1,7 @@
 import { readdir } from 'fs/promises';
 import getHymnBookInfo from './getHymnBookInfo';
 import { joinDataPath } from './joinDataPath';
+import getHymnsIndex from './getHymnsIndex';
 
 const getHymnBooks = async () => {
   const hymnBooksSlugs = await readdir(joinDataPath(''));
@@ -9,6 +10,7 @@ const getHymnBooks = async () => {
     hymnBooksSlugs.map(async (slug) => ({
       slug,
       name: (await getHymnBookInfo(slug)).name,
+      index: (await getHymnsIndex(slug)),
     }))
   );
 
