ba7e5bac7b13ace2f417525346db6d9b2957f96d
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2025-11-06 13:25:32 -0300

    feat: refactor hymn books ordering logic into context

--- context/HymnBooks.tsx
+++ context/HymnBooks.tsx
@@ -1,3 +1,4 @@
+import { HYMN_BOOKS_ORDER } from 'contants';
 import {
   createContext,
   Dispatch,
@@ -17,12 +18,35 @@ export const HymnBooksContext = createContext<
 
 export const useCreateHymnBooksCache = () => useState<HymnBooksState>(null);
 
+function orderHymnBooks(hymnBooks: HymnBook[] | null) {
+  if (!hymnBooks) return null;
+
+  return hymnBooks.sort((a, b) => {
+    const firstItemIndex = HYMN_BOOKS_ORDER.indexOf(a.slug);
+    const secondItemIndex = HYMN_BOOKS_ORDER.indexOf(b.slug);
+
+    if (firstItemIndex === -1) return 1;
+    if (secondItemIndex === -1) return -1;
+    if (firstItemIndex !== secondItemIndex) return firstItemIndex - secondItemIndex;
+
+    return a.name.localeCompare(b.name);
+  });
+}
+
 export const HymnBooksProvider = ({
   children,
   hymnBooksCache,
-}: PropsWithChildren<{ hymnBooksCache: ReturnType<typeof useCreateHymnBooksCache> }>) => (
-  <HymnBooksContext.Provider value={hymnBooksCache}>{children}</HymnBooksContext.Provider>
-);
+}: PropsWithChildren<{ hymnBooksCache: ReturnType<typeof useCreateHymnBooksCache> }>) => {
+  const [hymnBooks, setHymnBooks] = hymnBooksCache;
+
+  const orderedHymnBooks = orderHymnBooks(hymnBooks);
+
+  return (
+    <HymnBooksContext.Provider value={[orderedHymnBooks, setHymnBooks]}>
+      {children}
+    </HymnBooksContext.Provider>
+  );
+};
 
 export const useHymnBooks = () => useContext(HymnBooksContext);
 
